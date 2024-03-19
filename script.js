98ba7fc686836720a63a934333fe131399f561b6
Azhar Khan <azar123khan456@gmail.com>
2024-03-19 12:06:46 +0530

    added dark mode

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "./assests/icons/sun.png"
+
+    }else{
+        icon.src = "./assests/icons/moon.png"
+    }
+}
